dd98b1704cd1b6cbe6f32650647db7f5bc0208ff
Bob Jackson <wangyousong@9191offer.com>
2023-09-25 13:02:51 +0800

    init project

---
+++ src/test/java/com/wangyousong/practice/whatever/WhateverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangyousong.practice.whatever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhateverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
