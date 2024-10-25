528baf9246646f6ca1a1338dc3caed02ce799d25
Bob Jackson <wangyousong@9191offer.com>
2024-10-25 10:28:21 +0800

    @LocalServerPort

--- src/test/java/com/wangyousong/practice/whatever/WhateverApplicationTests.java
+++ src/test/java/com/wangyousong/practice/whatever/WhateverApplicationTests.java
@@ -2,12 +2,18 @@ package com.wangyousong.practice.whatever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WhateverApplicationTests {
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void contextLoads() {
+        System.out.println(port);
+        assert(port > 0);
     }
 
 }
